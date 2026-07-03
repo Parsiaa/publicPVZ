bf06f9677548f60bb8d16d79f4738ae4f911fdc4
Amirali zakeri <Amirzakeri457@gmail.com>
2026-07-03 22:22:49 +0330

    Completed Obstacle

--- src/models/Obstacle.java
+++ src/models/Obstacle.java
@@ -2,32 +2,48 @@ package models;
 
 import models.Enums.ObstacleType;
 
-public abstract class Obstacle extends Entity {
+public class Obstacle extends Entity {
     private ObstacleType type;
     private int currentHealth;
 
-    
     public Obstacle(ObstacleType type, int currentHealth) {
         this.type = type;
         this.currentHealth = currentHealth;
     }
 
+    @Override
+    public void takeDamage(int amount) {
+        this.currentHealth = Math.max(0, currentHealth - amount);
+        this.health = currentHealth;
+    }
+
+    public boolean isDestroyed() {
+        return currentHealth <= 0;
+    }
+
+    public void dropLoot(MatchState state) {
+        if (type == ObstacleType.GRAVE_SUN) {
+            state.addSun(50);
+            System.out.println("The grave dropped 50 sun!");
+        } else if (type == ObstacleType.GRAVE_FOOD) {
+            state.addPlantFood();
+            System.out.println("The grave dropped a plant food; you have " + state.getPlantFoods() + " plant foods now.");
+        }
+    }
+
     public ObstacleType getType() {
         return type;
     }
+
     public void setType(ObstacleType type) {
         this.type = type;
     }
+
     public int getCurrentHealth() {
         return currentHealth;
     }
+
     public void setCurrentHealth(int currentHealth) {
         this.currentHealth = currentHealth;
     }
-
-    
-    public void dropLoot(MatchState state) {
-        //TODO
-    }
-
 }
